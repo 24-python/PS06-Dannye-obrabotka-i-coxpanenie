ccd9af09a4c4661e85bc374fc98826d451da2d48
24-python <24_python@mail.ru>
2024-11-21 17:44:03 +0300

    1

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ rows = soup.find_all("tr")
 # tr - каждый ряд таблицы
 # td - каждая ячейка внутри ряда таблицы
 data = []
+
+for row in rows:
+    cols = row.find_all("td")
+    cleaned_cols = [col.text.strip() for col in cols]
+    data.append(cleaned_cols)
+
+print(data)
